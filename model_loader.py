2ba92a485d806bf1ebcd5bf180180c69ac70fa01
Mahi-Panchal <krupa9712660457@gmail.com>
2026-03-15 20:07:08 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,26 +1,32 @@
 import torch
-import gdown
 import os
+import requests
 import torch.nn as nn
 from torchvision import models
 
 MODEL_PATH = "waste_classifier.pth"
 
+FILE_ID = "https://drive.google.com/file/d/1poyQwfmxso842-2MeB8UOYmI61IJouxQ/view?usp=drive_link"
+
+DOWNLOAD_URL = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+
+
 def download_model():
 
     if not os.path.exists(MODEL_PATH):
 
-        file_id = "YOUR_FILE_ID_HERE"
+        response = requests.get(DOWNLOAD_URL)
 
-        url = f"https://drive.google.com/uc?id=https://drive.google.com/file/d/1poyQwfmxso842-2MeB8UOYmI61IJouxQ/view?usp=drive_link"
+        with open(MODEL_PATH, "wb") as f:
+            f.write(response.content)
 
-        gdown.download(url, MODEL_PATH, quiet=False, fuzzy=True)
 
 def load_model():
 
     download_model()
 
     model = models.resnet18(pretrained=False)
+
     model.fc = nn.Linear(model.fc.in_features, 6)
 
     model.load_state_dict(torch.load(MODEL_PATH, map_location="cpu"))
