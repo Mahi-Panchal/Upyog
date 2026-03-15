0e9948ca2b2ee6a6415275ed677e63ef4cf5d29e
Mahi-Panchal <krupa9712660457@gmail.com>
2026-03-15 20:02:00 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -10,21 +10,20 @@ def download_model():
 
     if not os.path.exists(MODEL_PATH):
 
-        file_id = "https://drive.google.com/file/d/1poyQwfmxso842-2MeB8UOYmI61IJouxQ/view?usp=sharing"
+        file_id = "YOUR_FILE_ID_HERE"
 
-        url = f"https://drive.google.com/uc?id={file_id}"
+        url = f"https://drive.google.com/uc?id=https://drive.google.com/file/d/1poyQwfmxso842-2MeB8UOYmI61IJouxQ/view?usp=drive_link"
 
-        gdown.download(url,MODEL_PATH,quiet=False)
+        gdown.download(url, MODEL_PATH, quiet=False, fuzzy=True)
 
 def load_model():
 
     download_model()
 
     model = models.resnet18(pretrained=False)
+    model.fc = nn.Linear(model.fc.in_features, 6)
 
-    model.fc = nn.Linear(model.fc.in_features,6)
-
-    model.load_state_dict(torch.load(MODEL_PATH,map_location="cpu"))
+    model.load_state_dict(torch.load(MODEL_PATH, map_location="cpu"))
 
     model.eval()
 
